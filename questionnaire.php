bebebef207755c1d51d63bbc44fa278d73f4229e
Sarah Stensland <sarahcs1202@gmail.com>
2022-06-06 10:19:04 -0400

    Set up questionnaire file

---
+++ questionnaire.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is going to be a brief example of how I think this file will work given the new coding ideas
+ * 
+ * @author Sarah Stensland
+ * @version 1.0
+ */
+
+// initialize everything
+$title = "Questionnaire";
+require_once 'header.php';
+
+// if the questionnaire hasn't been made yet
+if (!isset($_SESSION['questionnaire'])) {
+    // set up questionnaire
+} else {
+    // continue questionnaire
+} //end if
+
+?>
